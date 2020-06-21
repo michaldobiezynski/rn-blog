16ad4a20e03599e2ec09e8a34d9886a21737f8f7
michaldobiezynski <dobiezynski@gmail.com>
2020-06-21 21:04:44 +0100

    fixed typo

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -11,7 +11,7 @@ export default (reducer, actions, initialState) => {
     const boundActions = {};
 
     for (let key in actions) {
-      boundActions([key]) = actions[key](dispatch);
+      boundActions[key] = actions[key](dispatch);
     }
 
     return (
