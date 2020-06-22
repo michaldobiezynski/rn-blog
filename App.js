92ce23feae093e93f8d28a71a08fe9d6757682e6
michaldobiezynski <dobiezynski@gmail.com>
2020-06-22 08:29:23 +0100

    adding creation screen

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
+import { Provider } from "./src/context/BlogContext";
 import IndexScreen from "./src/screens/IndexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
-import { Provider } from "./src/context/BlogContext";
+import CreateScreen from "./src/screens/CreateScreen";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
     Show: ShowScreen,
+    Create: CreateScreen,
   },
   {
     initialRouteName: "Index",
