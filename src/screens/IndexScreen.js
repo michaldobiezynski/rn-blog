e8bd5e3116be57352a0948e770f176f968d0d451
michaldobiezynski <dobiezynski@gmail.com>
2020-06-22 09:01:42 +0100

    navigation on save

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -16,7 +16,6 @@ const IndexScreen = ({ navigation }) => {
 
   return (
     <View>
-      <Button title="Add Post" onPress={() => addBlogPost()} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
