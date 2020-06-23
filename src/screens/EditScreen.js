c0afea90f4897951fef9c4a03078e01f181c20a2
michaldobiezynski <dobiezynski@gmail.com>
2020-06-23 08:45:10 +0100

    extracting form logic

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,6 +1,8 @@
-import React, { useContext, useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+
 import { Context } from "../context/BlogContext";
+import BlogPostFrom from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -9,50 +11,9 @@ const EditScreen = ({ navigation }) => {
     (blogPost) => blogPost.id === navigation.getParam("id")
   );
 
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
-
-  return (
-    <View>
-      <Text>Post ID - {navigation.getParam("id")}</Text>
-      <Text style={styles.label}>Edit Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-        title="Save Changes"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate("Index");
-          });
-        }}
-      />
-    </View>
-  );
+  return <BlogPostFrom />;
 };
 
 export default EditScreen;
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 15,
-    padding: 5,
-    margin: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5,
-  },
-});
+const styles = StyleSheet.create({});
