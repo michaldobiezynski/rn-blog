dab0e69d20873f992caf63d7ba195eb8447562e5
michaldobiezynski <dobiezynski@gmail.com>
2020-06-25 08:15:19 +0100

    creating posts with post requests

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -39,8 +39,10 @@ const getBlogPosts = (dispatch) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: "add_blogpost", payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post("blogposts", { title, content });
+
+    // dispatch({ type: "add_blogpost", payload: { title, content } });
     callback && callback();
   };
 };
